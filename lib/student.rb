ac07b759f1308df7b8387018a9cb18e1321816d5
trevor dewindt <trevor.dewindt@gmail.com>
2022-12-14 19:30:11 -0700

    commit tree bfb25a756b23686ede43dcd3b0aab5daeead1239

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+require_relative './user'
 
+class Student < User
+    attr_accessor :knowledge
+
+    def initialize()
+        @knowledge = []
+    end
+
+    def learn(value)
+        @knowledge << value
+    end
 end
